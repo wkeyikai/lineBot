28a915b1369729dae4b610417d659d25f87e6203
aaron <aaron.wang@ascella.cc>
2025-11-21 14:07:24 +0800

    add env Variable and start script

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var linebot = require('linebot');
 // 用於辨識Line Channel的資訊
 var bot = linebot({ 
-    channelId: '1653596404', 
-    channelSecret: '50521ea1485ec4328202be11c98e0bfe', 
-    channelAccessToken: 'r+2i0hKVY8OND0+BBt24g3e8sfWo2rD3+kVLQcEV4f2weIPMX9EGpmTowjkQeB2RNUL4PPzHyCd7Ro8dudVUHG3rG2NCIudWi8Z4zbnwTH/VmSdYvburADDHl2y+EkCfl4HP6smfex4IAqfv9mf9mwdB04t89/1O/w1cDnyilFU='
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) { 
